ceb72ccc7a90e55fada10b905bbfd3f7d529759e
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-22 14:00:14 +0200

    format src/API files

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,52 +1,53 @@
 import cookie from 'cookie';
 
 import config from '../../config';
-import {backendRequestOptions} from "../helpers/cookie";
-import {IDemographics} from "../types/GlobalState";
+import { backendRequestOptions } from '../helpers/cookie';
+import { IDemographics } from '../types/GlobalState';
 
-const API_URL =  __DEVELOPMENT__ ? 'http://localhost:8000' : config('apiURL');
+const API_URL = __DEVELOPMENT__ ? 'http://localhost:8000' : config('apiURL');
 
 export const submitDemographics = (data: IDemographics) => {
-  return fetch(`${ API_URL }/api/demographics/`,
-    {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      credentials: "include",
+  return fetch(`${API_URL}/api/demographics/`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-  )
-}
-
+    credentials: 'include',
+  });
+};
 
 export const fetchAboutData = () => {
-  return fetch(`${ API_URL }/api/about?format=json`, {
+  return fetch(`${API_URL}/api/about?format=json`, {
     credentials: 'include',
-  })
-    .then(res => res.json())
-}
+  }).then(res => res.json());
+};
 
 export const fetchProfileData = (sessionKey: string) => {
-  return fetch(`${ API_URL }/api/profile/${ sessionKey }?format=json`, {
+  return fetch(`${API_URL}/api/profile/${sessionKey}?format=json`, {
     credentials: 'include',
-  })
-    .then(res => res.json())
-}
+  }).then(res => res.json());
+};
 
 export const fetchSessionData = (req?: any) => {
-  const options = __SERVER__ ? backendRequestOptions(req) : {
-    credentials: 'include',
-  };
-  return fetch(`${ API_URL }/api/index/?format=json`, options)
-    .then(res => res.json())
-}
+  const options = __SERVER__
+    ? backendRequestOptions(req)
+    : {
+        credentials: 'include',
+      };
+  return fetch(`${API_URL}/api/index/?format=json`, options).then(res =>
+    res.json()
+  );
+};
 
 export const getDatasetRecordings = (req?: any) => {
-  const options = __SERVER__ ? backendRequestOptions(req) : {
-    credentials: 'include',
-  };
-  return fetch(`${ API_URL }/api/download-audio/?format=json`, options)
-    .then(res => res.json())
-}
+  const options = __SERVER__
+    ? backendRequestOptions(req)
+    : {
+        credentials: 'include',
+      };
+  return fetch(`${API_URL}/api/download-audio/?format=json`, options).then(
+    res => res.json()
+  );
+};
--- src/api/users.ts
+++ src/api/users.ts
@@ -1,9 +1,7 @@
-
 export const fetchUsers = () => {
   return fetch('http://www.filltext.com/?rows=5&name={firstName}&pretty=true')
     .then(res => res.json())
     .then(json => {
-      return json
+      return json;
     });
-
-}
+};
