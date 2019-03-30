1beb32f67e6521353261484b551f1b1cadb46753
geekbahaa <geekbahaa1@gmail.com>
2019-03-30 23:48:52 +0200

    creating HMTL page

---
+++ src/pages/FollowAlong/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+class FollowAlong extends React.Component {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    )
+  }
+}
+
+export default FollowAlong;
+
