325d611d97df91334c1ee98f6c460616628c6840
geekbahaa <geekbahaa1@gmail.com>
2019-04-01 10:07:07 +0200

    Moving AudioStreamer.ts

--- src/pages/FollowAlong/index.tsx
+++ src/pages/FollowAlong/index.tsx
@@ -3,10 +3,13 @@ import { Icon } from 'react-icons-kit';
 import { circleONotch } from 'react-icons-kit/fa/circleONotch';
 import { micA } from 'react-icons-kit/ionicons/micA';
 import { stop } from 'react-icons-kit/fa/stop';
+import classNames from 'classnames';
+import io from 'socket.io-client';
 
 import { Container } from './styles';
 import Navbar from '../../components/Navbar';
 import RecordingButton from '../../components/RecordingButton';
+import AudioStreamer from '../../helpers/AudioStreamer'
 
 const testingAyahText = "قل هو الله احد";
 
@@ -17,6 +20,7 @@ interface IProps {
 interface IState {
   isRecording: boolean;
   isLoading: boolean;
+  showErrorMessage: boolean;
 }
 
 class FollowAlong extends React.Component<IProps, IState> {
@@ -25,7 +29,56 @@ class FollowAlong extends React.Component<IProps, IState> {
   state = {
     isLoading: false,
     isRecording: false,
+    showErrorMessage: false,
   }
+  handleRecordingButton = () => {
+    if (this.state.isLoading) {
+      return;
+    } else if (this.state.isRecording) {
+      this.handleStopRecording();
+    } else {
+      this.handleStartRecording();
+    }
+  };
+  handleRecordingError = e => {
+    this.setState({
+      showErrorMessage: true,
+    });
+    this.handleStopRecording();
+    console.log(e);
+  };
+  handleData = data => {
+    let interimTranscript = '';
+    if (data.results[0].isFinal) {
+      this.handleSearch();
+    } else {
+      interimTranscript += data.results[0].alternatives[0].transcript;
+    }
+
+    this.setState({
+      partialQuery: interimTranscript,
+    });
+  };
+  handleStartRecording = () => {
+    // resets the query string with new recordings
+    this.setState({
+      query: '',
+      isRecording: true,
+    });
+    this.AudioStreamer.initRecording(
+      this.handleData,
+      this.handleRecordingError
+    );
+  };
+  handleSearch = () => {
+
+  }
+  handleStopRecording = () => {
+    this.setState({
+      isRecording: false,
+    });
+    this.AudioStreamer.stopRecording();
+  };
   setLoading = (isLoading: boolean) => {
     this.setState({
       isLoading,
@@ -36,14 +89,12 @@ class FollowAlong extends React.Component<IProps, IState> {
       ? 'http://localhost:5000/'
       : 'https://tarteel-voice.now.sh/';
 
-    if (window.socket) {
-      this.socket = window.socket;
-    } else {
-      this.socket = io(speechServerURL);
-    }
+    this.socket = window.socket || io(speechServerURL);
 
     this.socket.on('loading', this.setLoading);
     this.socket.on('endStream', this.handleStopRecording);
+
+    this.scoekt.emit('setCurrentAyah', testingAyahText);
 
     this.AudioStreamer = new AudioStreamer(this.socket);
   }
@@ -59,13 +110,28 @@ class FollowAlong extends React.Component<IProps, IState> {
     return (
       <Container>
         <Navbar />
+        {this.state.showErrorMessage ? (
+          <RecordingError
+            onClose={() => {
+              this.setState({ showErrorMessage: false });
+            }}
+          />
+        ) : null}
         <div className={'ayah'} >
           <div className="text">
             {
               testingAyahText.split(' ')
             }
           </div>
-          <RecordingButton>
+          <div className="words">
+            <span className={'query'}>{this.state.query}</span>
+            &nbsp;
+            <span className="partial-query">{this.state.partialQuery}</span>
+          </div>
+          <RecordingButton
+            className={`mic ${classnames}`}
+            onClick={this.handleRecordingButton}
+          >
             {
               this.state.isLoading ? (
                 <div className={'icon spin'}>
