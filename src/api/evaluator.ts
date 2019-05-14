02ed7bd390f70bd307f6f0333d0ded45b9bb7703
Anas Abou Allaban <anas@abouallaban.info>
2019-05-14 00:31:08 -0400

    - Added CORS

--- src/api/evaluator.ts
+++ src/api/evaluator.ts
@@ -1,16 +1,17 @@
 import config from '../../config';
 import { backendRequestOptions } from '../helpers/cookie';
 
-const API_URL = config('apiURL');
+const API_URL = __DEVELOPMENT__ ? 'http://localhost:8000' : config('apiURL');
 
 export const fetchEvaluatorAyah = (req?: any) => {
-  const options = __SERVER__
-    ? backendRequestOptions(req)
-    : {
-        credentials: 'include',
-      };
+  const options = __SERVER__ ?
+    backendRequestOptions(req) :
+    {
+      credentials: 'include',
+      mode: 'cors',
+    };
   return fetch(`${API_URL}/v1/evaluator/?format=json`, options).then(
-    (res: Response) => res.json()
+    (res: Response) => res.json(),
   );
 };
 
@@ -62,6 +63,6 @@ export const fetchSpecificEvaluatorAyah = (
       } else {
         return res.json();
       }
-    }
+    },
   );
 };
